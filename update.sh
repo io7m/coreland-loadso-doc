053b3df018044c4e7936813ce6708621d6b5398a
m0 <m0@viper>
2010-07-07 00:33:46 +0000

    Cleanup

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 make clean html-split html-single css txt package release &&
-  rsync -avz --delete release/ /shared_tmp/loadso-doc-release/
+  rsync -avz --delete release/ www:/sites/waste/doc/loadso-doc-release/
